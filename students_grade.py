000983f34e5262fca3966f1211d98f5dd35b48eb
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-09 16:14:54 +0530

    Update students_grade.py

--- students_grade.py
+++ students_grade.py
@@ -2,9 +2,9 @@
 student_grade = int(input("marks: "))
 if (student_grade >= 90):
     print("Student is in grade A")
-elif (student_grade >= 80):
+elif (student_grade >= 80 and student_grade < 90):
     print("Student is in grade B")
-elif (student_grade >= 70):
+elif (student_grade >= 70 and student_grade < 80):
     print("student is in grade C")
 else:
     print("student is in grade D")  
