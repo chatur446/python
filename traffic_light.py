6b78e8690466cd6c18d10cca1f94f76c6a5ec0a1
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-09 15:41:21 +0530

    Create traffic_light.py

---
+++ traffic_light.py
@@ -0,0 +1,13 @@
+# conditional statement traffic light problem
+light_color = input("light: ")
+if(light_color == "red"):
+    print("stop")
+elif(light_color =="green"):
+    print("go")
+elif(light_color == "yellow"):
+    print("be ready")
+elif(light_color == "blue"):
+    print("light is broken")
+else:
+    print("no idea what color it is")        
+        
