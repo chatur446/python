9f3f569ffa97e342397129ec59625ee2ac31b2ba
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-12 15:43:47 +0530

    avg_of_num.py
    Average of two numbers

---
+++ avg_of_num.py
@@ -0,0 +1,6 @@
+#write a program to inpt 2 floating point numbers and print their average
+
+num1 = float(input("enter num1: "))
+num2 = float(input("enter num2: "))
+avg = ((num1 + num2)/2)
+print("Average is = ",avg)
