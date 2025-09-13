5d18862098973b7a04c1669cd57b6f8d1f41788d
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-13 21:24:13 +0530

    Greater_then.py
    taking two number input and printing one is greater then other

---
+++ Greater_then.py
@@ -0,0 +1,10 @@
+#write a program to input 2 int number , a and b. 
+#print true if a is greater than or equal to b. if not print false
+
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+# if(a>=b):
+#     print("True")
+# else:
+#     print("False") 
+print(a>=b)
