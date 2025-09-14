d98d6c45117f2e4f47eb157357bd98bc51b85305
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-14 21:52:37 +0530

    Update greatest_of_three_no.py

--- greatest_of_three_no.py
+++ greatest_of_three_no.py
@@ -3,9 +3,9 @@
 num1 = int(input("enter first number: "))
 num2 = int(input("enter second number: "))
 num3 = int(input("enter third number: "))
-if (num1>num2 and num1>num3):
+if (num1>=num2 and num1>=num3):
     print("greatest number is: ",num1)
-elif(num2>num1 and num2>num3):
+elif(num2>=num3):
     print("greatest number is: ",num2)
 else:
     print("greatest number is: ",num3)
