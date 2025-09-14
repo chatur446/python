4035f9b4f7d37d3e02ac06323919123ea32207f4
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-14 20:14:20 +0530

    length_of_string.py

---
+++ length_of_string.py
@@ -0,0 +1,8 @@
+# Question:- Write a program to input user's first name and print its length
+
+first_name = input("first_name: ")
+str1 = first_name
+str2 = "chaturvedi"
+final_str = str1 + " " + str2
+print(final_str)
+print(len(str1))
