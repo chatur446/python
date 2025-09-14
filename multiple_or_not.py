cefa34e88bd40126e0277842df1000837bb95fe3
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-14 22:06:42 +0530

    multiple_or_not.py

---
+++ multiple_or_not.py
@@ -0,0 +1,7 @@
+## write a program to check if a number is a  multiple of 7 or not
+
+num = int(input("enter number: "))
+if (num%7==0):
+    print(num,"is multiple of 7")
+else:
+    print(num,"is not multiple of 7")     
