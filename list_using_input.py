16b13b75fc04334fd24ee646ca1374b14e49af1e
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-15 23:20:10 +0530

    list_using_input.py

---
+++ list_using_input.py
@@ -0,0 +1,8 @@
+# write a program to ask the user to enter the name of their fav.
+# movies and store them in a list
+
+list = [" "," "," "]
+list[0] = str(input("enter first movie: "))
+list[1] = str(input("enter second movie: "))
+list[2] = str(input("enter third movie: "))
+print(list)
