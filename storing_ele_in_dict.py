27eab96866742c066376335b1f2a2db78ab082b3
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-25 14:52:37 +0530

    storing_ele_in_dict.py

---
+++ storing_ele_in_dict.py
@@ -0,0 +1,9 @@
+# store following word meanings in a python dictionary:
+'''table:"a piece of furniture","list of facts and figures"
+cat:"a small animal"'''
+
+collection = {
+    "cat" : "a small animal",
+    "table" : ("a piece of furniture","list of facts and figure")
+}
+print(collection)
