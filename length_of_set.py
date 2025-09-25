1cc05063aab050359847b739b6b4bc0e42de36a9
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-25 19:39:21 +0530

    length_of_set.py

---
+++ length_of_set.py
@@ -0,0 +1,6 @@
+# question:- you are given a list of subjects for students. Assume one classrooom is required for 1 subject.
+# how many classroom are need by all students.
+# "python","java","cpp","python","javascript","java","python","java","cpp","c"
+
+subject = {"python","java","cpp","python","javascript","java","python","java","cpp","c"}
+print("class needed foe all the students are:", len(subject))
