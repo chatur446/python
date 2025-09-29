6f0b4f9e0c365c02877b94ada292b7e1f569d75b
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-09-29 12:52:10 +0530

    Create diff_datatype_same_num_stored_in_set.py

---
+++ diff_datatype_same_num_stored_in_set.py
@@ -0,0 +1,8 @@
+# queston:- figure out a way to store 9 and 9.0 as separate values in the set.
+#           (you can take help of built-in data types)
+
+values = {
+    ("float",9.0),
+    ("int",9)
+}
+print(values)
