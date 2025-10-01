704e8ce1ecdaf1f37a89ffc6509458a7331fe90c
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-10-01 20:12:03 +0530

    Create functions_q1.py

---
+++ functions_q1.py
@@ -0,0 +1,9 @@
+# write a function to print the length of the list
+
+list1 = [2,4,6,2,7,3]
+list2 = ["nice","hello","welcome","come","good"]
+
+def print_list(list):
+    print(list)
+
+print_list(list2)
