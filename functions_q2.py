c8126e35df874c8754ced54e250f52795e9055e7
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-10-05 14:00:02 +0530

    Create functions_q2.py

---
+++ functions_q2.py
@@ -0,0 +1,12 @@
+# write a function to print the element of the list in a single line.
+# (list is the parameter)
+
+list1 = ["nice","hello","welcome","sayonara","arigato"]
+list2 = [2,5,3,6,8,0,4]
+
+def print_element(list):
+    for item in list:
+        print(item ,end =" ")
+
+
+print_element(list1)
