d9843404e3cdc1b1af96c24d28108289d7803a7e
Ayush Chaturvedi <ayushchaturvedi1305@gmail.com>
2025-10-05 14:34:13 +0530

    Create functions_hq5.py

---
+++ functions_hq5.py
@@ -0,0 +1,11 @@
+# write a function takes a numbers as input , if the number is odd 
+# then the function should print a string say "odd", same goes for even.
+
+def odd_even(n):
+    if (n%2==0):
+        print("even")
+    else:
+        print("odd")
+
+
+odd_even(54)
